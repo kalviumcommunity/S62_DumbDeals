414a6a6048c4396b66c7ed3e51f674b85da1d98e
varshapanda <varsha.vijayashwini@kalvium.community>
2024-12-21 11:27:07 +0530

    mongoAtlas-Connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+// const app = express();
+// const port = 3000;
+const dotenv = require('dotenv');
+const connectDatabase = require('./DB/database.js');
+
+dotenv.config({ path: './.env' });
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.get('/ping',(request,response)=>{
     response.send('Hello World!');
 });
 
 app.listen(port,()=>{
+    connectDatabase();
     console.log(`Your server is running on http://localhost:${port}`);
 });
