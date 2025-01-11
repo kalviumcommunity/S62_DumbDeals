ca0e033ab6aad7aba2332b21ed818191545d65a2
varshapanda <varsha.vijayashwini@kalvium.community>
2025-01-12 02:40:47 +0530

    completed-ASAP Project - Modifying data through API

--- routes/users.route.js
+++ routes/users.route.js
@@ -54,7 +54,7 @@ app.delete('/:id', async(req,res)=>{
         return res.status(200).json({message:"User data deleted successfully", data:deleteUserData});
     }
     catch(error){
-        return res.status(404).json({message:error.message});
+        return res.status(500).json({message:error.message});
     }
 })
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const {getDB,connection} = require('./DB/mongo-client.js');
 
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', async (req, res) => {
     const checkStatus = await connection.connect();
